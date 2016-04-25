1e66019675e3ae72a2920d1c862e69a8cd0188c8
faetea <saphron.scul@gmail.com>
2016-04-25 18:56:48 -0400

    collection_id now getting set upon Art upload, issue was in Rails not Ember, :collection_id was not permitted in (art_params) in Art controller

--- app/collection/route.js
+++ app/collection/route.js
@@ -4,11 +4,7 @@ export default Ember.Route.extend({
 
   actions: {
     createArt (art) {
-      let currentCollection = this.modelFor('collection');
-      let newArt = this.get('store').createRecord('art', art);
-      newArt.set('collection', currentCollection);
-      console.log(newArt.get('collection').get('id'));
-      newArt.save();
+      this.get('store').createRecord('art', art).save();
     },
 
   }
