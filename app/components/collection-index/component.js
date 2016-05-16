72393a4a86838632bbf2bdfbd8fa0d6d26df5b1c
faetea <saphron.scul@gmail.com>
2016-05-16 02:36:23 -0400

    wrote a nameOrEmail computed property, so if user has created username it will be displayed instead of their email.

--- app/components/collection-index/component.js
+++ app/components/collection-index/component.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  nameOrEmail: Ember.computed('collection.user.username', 'collection.user.email', function(){
+    return (this.get('collection.user.username')||this.get('collection.user.email'));
+  })
 });
--- app/components/collection-show/component.js
+++ app/components/collection-show/component.js
@@ -6,6 +6,9 @@ export default Ember.Component.extend({
 
     return Ember.String.htmlSafe(style);
   }),
+  nameOrEmail: Ember.computed('collection.user.username', 'collection.user.email', function(){
+    return (this.get('collection.user.username')||this.get('collection.user.email'));
+  })
 });
 
 // Ember.set('myStyle', Ember.String.htmlSafe("color: blue;"));
