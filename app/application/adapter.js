1a6e04a5bc306b7208756a0de9397c2cbbf456d9
faetea <saphron.scul@gmail.com>
2016-06-04 15:00:06 -0400

    defined/imported ENV

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import ActiveModelAdapter from 'active-model-adapter';
+import ENV from 'ember-imagegallery/config/environment';
 
 export default ActiveModelAdapter.extend({
   auth: Ember.inject.service(),
